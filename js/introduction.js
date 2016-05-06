faefe06e745395f5dd4b245c90ad9fb0fe06efe6
xiaoguoping <i10630226@163.com>
2016-05-06 15:47:15 +0800

    成员介绍页面
    成员介绍页面

---
+++ js/introduction.js
@@ -0,0 +1,32 @@
+$(function(){	
+	
+    var x = 22;
+	var y = 20;
+		
+	$("a.smallimage").hover(function(e){
+		$("body").append('<p id="bigimage"><img src="'+ this.rel + '" alt="" /></p>');
+        $(this).find('img').stop().fadeTo('slow',0.5);		
+		widthJudge(e);
+	    $("#bigimage").fadeIn('fast');
+	},function(){
+	    $(this).find('img').stop().fadeTo('slow',1);
+		$("#bigimage").remove();
+    });	
+	
+	$("a.smallimage").mousemove(function(e){
+		widthJudge(e);
+	});	
+	
+    function widthJudge(e){
+		var marginRight = document.documentElement.clientWidth - e.pageX; 
+		var imageWidth = $("#bigimage").width();
+		if(marginRight < imageWidth)
+		{
+		    x = imageWidth + 22;
+			$("#bigimage").css({top:(e.pageY - y ) + 'px',left:(e.pageX - x ) + 'px'});	
+		}else{
+		    x = 22;
+		    $("#bigimage").css({top:(e.pageY - y ) + 'px',left:(e.pageX + x ) + 'px'});
+        };	
+	}
+});
